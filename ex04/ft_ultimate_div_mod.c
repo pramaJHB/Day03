95ee96829694fc62509f303c2cdb433eb45d13db
Pravir Rama <prama@c4r9s1.wethinkcode.co.za>
2020-06-23 12:38:11 +0200

    Completed ex04

--- ex04/ft_ultimate_div_mod.c
+++ ex04/ft_ultimate_div_mod.c
@@ -6,9 +6,14 @@
 /*   By: prama <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/22 10:30:16 by prama             #+#    #+#             */
-/*   Updated: 2020/06/22 11:49:50 by prama            ###   ########.fr       */
+/*   Updated: 2020/06/23 12:37:41 by prama            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/*
+#include <stdio.h>
+#include <unistd.h>
+*/
 
 void	ft_ultimate_div_mod(int *a, int *b)
 {
@@ -20,5 +25,20 @@ void	ft_ultimate_div_mod(int *a, int *b)
 
 	*a = div;
 	*b = mod;	
-
 }
+/*
+int	main(void)
+{
+	int a;
+	int b;
+
+	a = 157;
+	b = 10;
+
+	ft_ultimate_div_mod(&a, &b);
+
+	printf("%d\n", a);
+	printf("%d\n", b);
+
+	return (0);
+}*/
