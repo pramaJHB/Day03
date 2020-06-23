2d73301641ccc371315fc83e50c7a9ba1bf87706
Pravir Rama <prama@c4r9s1.wethinkcode.co.za>
2020-06-23 12:31:19 +0200

    Completed ex03

--- ex03/ft_div_mod.c
+++ ex03/ft_div_mod.c
@@ -6,12 +6,35 @@
 /*   By: prama <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/22 09:54:23 by prama             #+#    #+#             */
-/*   Updated: 2020/06/22 10:08:05 by prama            ###   ########.fr       */
+/*   Updated: 2020/06/23 12:30:52 by prama            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/*
+#include <stdio.h>
+#include <unistd.h>
+*/
 
 void	ft_div_mod(int a, int b, int *div, int *mod)
 {
 	*div = a / b;
 	*mod = a % b;
 }
+/*
+int	main(void)
+{
+	int a;
+	int b;
+	int div;
+	int mod;
+
+	a = 175;
+	b = 10;
+
+	ft_div_mod(a, b, &div, &mod);
+
+	printf("%d\n", div);
+	printf("%d\n", mod);
+	
+	return (0);
+}*/
