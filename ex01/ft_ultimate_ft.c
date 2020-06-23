ba9b3f745d1d9c2d18841a8d56e7b62183cb2159
Pravir Rama <prama@c4r9s1.wethinkcode.co.za>
2020-06-23 12:10:39 +0200

    Completed ex01

--- ex01/ft_ultimate_ft.c
+++ ex01/ft_ultimate_ft.c
@@ -6,11 +6,47 @@
 /*   By: prama <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/22 09:32:30 by prama             #+#    #+#             */
-/*   Updated: 2020/06/22 10:00:55 by prama            ###   ########.fr       */
+/*   Updated: 2020/06/23 12:10:05 by prama            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/*
+#include <stdio.h>
+#include <unistd.h>
+*/
 
 void	ft_ultimate_ft(int *********nbr)
 {
 	*********nbr = 42;
 }
+/*
+int	main(void)
+{
+	int a;
+	int	*ptr;
+	int **ptr2;
+	int ***ptr3;
+	int ****ptr4;
+	int *****ptr5;
+	int ******ptr6;
+	int *******ptr7;
+	int ********ptr8;
+	int *********ptr9;
+
+	a = 17;
+	ptr  = &a;
+	ptr2 = &ptr;
+	ptr3 = &ptr2;
+	ptr4 = &ptr3;
+	ptr5 = &ptr4;
+	ptr6 = &ptr5;
+	ptr7 = &ptr6;
+	ptr8 = &ptr7;
+	ptr9 = &ptr8;
+
+	ft_ultimate_ft(ptr9);
+
+	printf("%d\n", a);
+	return (0);
+}
+*/
